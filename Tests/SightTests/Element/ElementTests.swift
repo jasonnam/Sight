43857aeb95e200c086da015136defbe47dd99617
Federico Zanetello <zntfdr@gmail.com>
2020-03-12 14:32:29 +0700

    add testInstanceDistance

--- Tests/SightTests/Element/ElementTests.swift
+++ Tests/SightTests/Element/ElementTests.swift
@@ -2,14 +2,14 @@
 import XCTest
 
 final class ElementTests: XCTestCase {
-  func testElementValue() {
+  func testInstanceValue() {
     for value in 1...10 {
       let element = Element(value: value, position: .zero)
       XCTAssertEqual(element.value, value)
     }
   }
 
-  func testElementPosition() {
+  func testInstancePosition() {
     // Verify all possible combinations of positive/negative values.
     let positions: [SIMD2<Float>] = [
       .zero,
@@ -26,6 +26,16 @@ final class ElementTests: XCTestCase {
     for position in positions {
       let element = Element(value: (), position: position)
       XCTAssertEqual(element.position, position)
+    }
+  }
+
+  func testInstanceDistance() {
+    let position = SIMD2<Float>(x: 0, y: 1)
+
+    for distance in 0...10 {
+      let element = Element(value: (), position: .zero)
+      let distanceFromElement = element.distance(from: position * Float(distance))
+      XCTAssertEqual(distanceFromElement, Float(distance))
     }
   }
 }
