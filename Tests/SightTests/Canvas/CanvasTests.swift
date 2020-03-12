d72a0acdb8275db8d0c59174b3dcb65f1b70b206
Federico Zanetello <zntfdr@gmail.com>
2020-03-12 16:46:44 +0700

    add testTwoValuesCompete

--- Tests/SightTests/Canvas/CanvasTests.swift
+++ Tests/SightTests/Canvas/CanvasTests.swift
@@ -29,10 +29,33 @@ final class CanvasTests: XCTestCase {
 
     canvas.add(value, at: valuePosition)
 
+    // Just out of area position.
     let outOfAreaSearchPosition = valuePosition - SIMD2<Float>(x: 1, y: 0) * minCellSize / 2 - 0.00001
     XCTAssertNil(canvas.closestValue(to: outOfAreaSearchPosition))
 
+    // On the edge position.
     let edgeSearchPosition = valuePosition - SIMD2<Float>(x: 1, y: 0) * minCellSize / 2
     XCTAssertNotNil(canvas.closestValue(to: edgeSearchPosition))
+  }
+
+  func testTwoValuesCompete() {
+    let canvas = Canvas<Int>(minBounds: .zero, maxBounds: .one, minimumCellSize: 1)
+    let zeroValue = 0
+    let oneValue = 1
+    let oneValuePosition = SIMD2<Float>(x: 1, y: 0)
+    canvas.add(zeroValue, at: .zero)
+    canvas.add(oneValue, at: oneValuePosition)
+
+    // From zero to halfway we should get `zeroValue`.
+    for i in 0...5 {
+      let position = SIMD2<Float>(x: 0.1 * Float(i), y: 0)
+      XCTAssertEqual(canvas.closestValue(to: position), zeroValue)
+    }
+
+    // From halfway till the end we should get `oneValue`.
+    for i in 6...10 {
+      let position = SIMD2<Float>(x: 0.1 * Float(i), y: 0)
+      XCTAssertEqual(canvas.closestValue(to: position), oneValue)
+    }
   }
 }
