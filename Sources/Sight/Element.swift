1fc4870ffcc209342c83b63b253bebbabc7a7d65
Federico Zanetello <zntfdr@gmail.com>
2020-03-12 14:21:43 +0700

    add Element documentation

--- Sources/Sight/Element.swift
+++ Sources/Sight/Element.swift
@@ -1,18 +1,33 @@
 import Foundation
 
+/// A Canvas element.
+///
+/// Represents an element in the canvas.
 final class Element<T>: NSObject {
+  /// Value associated with the element.
   let value: T
+
+  /// Canvas coordinate of the element.
   let position: SIMD2<Float>
 
+  /// Initializes a new `Element` instances with the given parameters.
+  ///
+  /// - Parameters:
+  ///   - value: A value associated with this element.
+  ///   - position: The coordinate of this element in the canvas.
   init(value: T, position: SIMD2<Float>) {
     self.value = value
     self.position = position
     super.init()
   }
 
-  func distance(from point: SIMD2<Float>) -> Float {
-    let xDelta: Float = position.x - point.x
-    let yDelta: Float = position.y - point.y
+  /// Computes the distance between this instance and the given coordinate.
+  ///
+  /// - Parameter position: Coordinate of the point we would like to compute the
+  ///    distance of.
+  func distance(from position: SIMD2<Float>) -> Float {
+    let xDelta: Float = self.position.x - position.x
+    let yDelta: Float = self.position.y - position.y
     return sqrt(pow(xDelta, 2) + pow(yDelta, 2))
   }
 }
