856d006a547ce096c14804980bea55aedc95e59a
Federico Zanetello <zntfdr@gmail.com>
2020-03-15 08:21:40 +0700

    set cell size to 1

--- Sources/Sight/Region.swift
+++ Sources/Sight/Region.swift
@@ -32,7 +32,7 @@ public struct Region<T> {
   /// - SeeAlso: `init(quadMin:maxBounds:searchRadius:)`.
   init(boundingQuad quad: GKQuad, searchRadius: Float) {
     self.searchRadius = searchRadius
-    tree = GKQuadtree(boundingQuad: quad, minimumCellSize: searchRadius)
+    tree = GKQuadtree(boundingQuad: quad, minimumCellSize: 1)
   }
 
   /// Adds the value at the specified position.
